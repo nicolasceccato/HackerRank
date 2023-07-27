437066563aa5d6a1e61632c0809404e61cac3489
nicolasceccato <nicolasceccato@hotmail.com>
2023-07-26 21:37:37 -0300

    exercicio basico hackerrank

---
+++ exercicio3/untitled/src/Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(scanner.nextInt());
+        }
+        scanner.close();
+
+    }
+}
